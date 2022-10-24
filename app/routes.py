4572c3e41dae6fa22836b0fd9a32aab231a3070f
ttodawg01 <tonycampos1803@gmail.com>
2022-10-24 16:31:34 -0500

    halfway

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,15 @@
+from flask import render_template
 from app import app
 
 #create a route using the @app.route to trigger function based on endpoint
 @app.route("/")
 def index():
-    return "hello this is the index route"
+    user_info = {
+        'username':'tonyc',
+        'email':'tonycampos@gmail.com',
+    }
+    colors = ['red', 'orange', 'blue']
+    return render_template('index.html', user=user_info, colors = colors)
 
 @app.route('/posts')
 def posts():
